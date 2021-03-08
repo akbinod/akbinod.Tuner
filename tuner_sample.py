0eac3d3f4986316e7ddf762b98a977271834549d
Binod Purushothaman <binod@gatech.edu>
2021-03-08 15:43:25 -0500

    update sample

--- tuner_sample.py
+++ tuner_sample.py
@@ -17,15 +17,24 @@ def draw_circle(image, radius, color, center, tuner):
     return image
 
 def simple_draw_circle():
+    '''
+    Demonstrates using tuples, lists and dicts to init Tuner.
+    '''
     img = cv2.imread(img_sample_1)
+
+    # circles can be drawn in one of these colors
     colors = {
         "red" : (0,0,255)
         , "green": (0,255,0)
         , "blue": (255,0,0)
     }
+    # the center will be one of these vals
     centers = [(200,200), (300,300), (400,400), (500,500)]
+    # radius will max out at 100, have a min val of 20 and default to 50
+    mmd = (100,20,50)
+
     # this launches the tuner
-    img = draw_circle(img,100,colors,centers)
+    img = draw_circle(img,mmd,colors,centers)
 
     return
 
